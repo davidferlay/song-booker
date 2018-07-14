1a8b64f146eb30d302fe51055a67d6f5cea1c60c
David Ferlay <davidferlay@outlook.com>
2018-07-14 17:49:30 +0200

    Endpoint of get request creation

--- server/src/app.js
+++ server/src/app.js
@@ -7,8 +7,16 @@ const morgan = require('morgan')
 
 // Building of express app server
 const app = express()
-app.use(morgan('combine')) // Log
+app.use(morgan('combined')) // Log
 app.use(bodyParser.json()) // Parse json easily
 app.use(cors()) // Ressource request control
 
+// Endpoint of get request
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+// Browser access
 app.listen(process.env.PORT || 8081)
