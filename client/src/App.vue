04401cc55265d404737455bde9691c88912a8be6
David Ferlay <davidferlay@outlook.com>
2018-07-16 00:18:32 +0200

    Added menu links with routes to header

--- client/src/App.vue
+++ client/src/App.vue
@@ -24,7 +24,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif, Material+Icons;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
