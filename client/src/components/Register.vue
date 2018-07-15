fc6b40c982007faff1833737c2c4a47adfccab6b
David Ferlay <davidferlay@outlook.com>
2018-07-15 21:16:56 +0200

    Implemented material design with validation on register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,31 +1,57 @@
 <template>
-  <div>
-    <h1>Register on this page</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-      <br>
-      <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-      <br>
-      <div class="error" v-html="error" />
-      <br>
-      <button @click="register">
-        Register
-      </button>
-  </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Register form</v-toolbar-title>
+                <v-spacer></v-spacer>
+                </v-toolbar>
+                <v-card-text>    
+                  <v-form 
+                    name="Songbooker register form">
+                    <v-text-field
+                      label="Email"
+                      v-model="email"
+                      type="email"
+                      prepend-icon="person"
+                      class="field">
+                      autofocus
+                    </v-text-field>
+                    <br>
+                    <v-text-field
+                      label="Password"
+                      type="password"
+                      v-model="password"
+                      prepend-icon="lock"
+                      autocomplete="new-password"
+                      class="field">
+                    </v-text-field>
+                  </v-form>
+                <br>
+                <div class="danger-alert" v-html="error" />
+                <br>
+                <v-btn
+                  dark
+                  class="cyan"
+                  @click="register">
+                  Register
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
-<!-- v-model = Magic keyword for passing and getting data -->
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
+
 export default {
-  name: 'Register',
   data () {
     return {
       email: '',
@@ -36,9 +62,14 @@ export default {
   methods: {
     async register () {
       try {
-        await AuthenticationService.register({
+        const response = await AuthenticationService.register({
           email: this.email,
           password: this.password
+        })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
+        this.$router.push({
+          name: 'songs'
         })
       } catch (error) {
         this.error = error.response.data.error
@@ -50,21 +81,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.error {
-  color:red;
-}
+
+
 </style>
