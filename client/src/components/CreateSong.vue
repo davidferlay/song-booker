dca5909ad17b156bb192ff079adf12a7e48bbb74
David Ferlay <davidferlay@outlook.com>
2018-07-28 00:38:52 +0200

    Updated layout of song create page

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -1,18 +1,25 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">12</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 2" :key="`6${i}`" xs6>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">6</v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <v-container grid-list-md text-xs-center>
+        <v-layout wrap>
+            
+        <v-flex d-flex>
+            <v-card dark color="primary">
+                <v-card-text class="px-0">left</v-card-text>
+            </v-card>
+
+            <v-card dark color="primary">
+               <v-card-text class="px-0">right</v-card-text>
+            </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+            <v-card dark color="primary">
+            <v-card-text class="px-0">bottom</v-card-text>
+            </v-card>
+        </v-flex>
+
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
