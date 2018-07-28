e6f52f5d3216c32b1fe6db53440175b1b2965032
David Ferlay <davidferlay@outlook.com>
2018-07-28 17:42:03 +0200

    Added songs vue styles and button/route to song pages

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,6 +2,7 @@
   <v-app id="inspire">
     <v-content>
             <panel title="Songs">
+              
                 <v-btn
                   slot="action"
                   @click="navigateTo({name: 'songs-create'})"
@@ -14,11 +15,42 @@
                   fab>
                   <v-icon>add</v-icon>
                 </v-btn>
-                <div v-for="song in songs"
+
+                <div
+                  class="song" 
+                  v-for="song in songs"
                   :key="song.id">
-                  {{song.title}} -
-                  {{song.artist}} -
-                  {{song.album}}
+                  <v-layout>
+
+                    <v-flex xs6>
+                      <div class="song-title">
+                        {{song.title}}
+                      </div>
+                      <div class="song-artist">
+                        {{song.artist}}
+                      </div>
+                      <div class="song-genre">
+                        {{song.genre}}
+                      </div>
+                      <v-btn
+                          dark
+                          class="cyan"
+                          @click="navigateTo({
+                            name: 'song',
+                            params: {
+                              songId: song.id
+                            }
+                          })">
+                          See
+                      </v-btn>
+                    </v-flex>
+
+                    <v-flex xs6>
+                      <img class="album-image" 
+                      :src="song.albumImageUrl">
+                    </v-flex>
+
+                  </v-layout>
                 </div>
             </panel>
     </v-content>
@@ -50,5 +82,25 @@ export default {
 </script>
 
 <style scoped>
-
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-weight: normal;
+  font-size: 30px;
+}
+.song-artist {
+  font-weight: normal;
+  font-size: 24px;
+}
+.song-genre {
+  font-weight: normal;
+  font-size: 18px;
+}
+.album-image {
+  width: 75%;
+  margin: 0 auto;
+}
 </style>
