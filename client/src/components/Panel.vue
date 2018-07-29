e5ca43fde83bf82bd9dca06ea5d21decdd03a41f
David Ferlay <davidferlay@outlook.com>
2018-07-29 13:54:53 +0200

    Restructured components and added first part of song vue template code

--- client/src/components/Panel.vue
+++ client/src/components/Panel.vue
@@ -3,7 +3,6 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
               <v-toolbar dark dense color="primary">
                 <v-toolbar-title>{{title}}</v-toolbar-title>
@@ -14,14 +13,11 @@
                   No content in slot
               </slot>
             </v-card>
-          </v-flex>
         </v-layout>
       </v-container>
     </v-content>
   </v-app>
 </template>
-// To write your html
-// You can insert v- attributes (v-model, v-html and others) here to bind method of script section
 
 <script>
 export default {
